8dec40cd86a99e68ee82e0af87a0046b79886574
souhail <lightshadow416@gmail.com>
2025-05-25 00:48:04 +0100

    fix(loading) : added a proper loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,10 @@
+import Image from "next/image";
+import loadingImg from "/public/preloader.svg";
+
 export default function Loading() {
   return (
-    <div className="loader-background">
-      <span className="loader">
-        <span className="loader-inner"></span>
-      </span>
+    <div className="flexCenter h-screen">
+      <Image src={loadingImg} alt="loading img" height={140} width={140} />
     </div>
   );
 }
